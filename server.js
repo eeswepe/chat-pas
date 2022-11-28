6d6502adfefe9aa878be9a982969a61acd333d26
Deavennn <singgihwahyupermana@gmail.com>
2022-11-28 19:14:00 +0700

    added my project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    socket.broadcast.emit('message', data);
+  });
+});
+
+server.listen(8000, () => {
+  console.log('server connected');
+});
